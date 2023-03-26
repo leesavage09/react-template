08fe820492df93924943b2052584c0530d222567
Lee <leesavage09@gmail.com>
2023-03-26 21:15:36 +0300

    ci: configure prettier

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button
-          type="button"
-          onClick={() => setCount((currentCount) => currentCount + 1)}
-        >
+        <button type="button" onClick={() => setCount((currentCount) => currentCount + 1)}>
           {`count is ${count}`}
         </button>
         <p>
-          Edit
-          {' '}
-          <code>src/App.jsx</code>
-          {' '}
-          and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
