6d4ba529a2be7ea991509938386a862f667e236d
Lee <leesavage09@gmail.com>
2023-03-26 21:00:25 +0300

    ci: remove jsconfig and replace with vite resolve alias config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import jsconfigPaths from "vite-jsconfig-paths";
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  plugins: [react()],
 });
